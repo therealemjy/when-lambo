2518586e0d6c35bc8124518a585dae8fb295d9a4
therealemjy <mjulian@ciedigital.com>
2021-11-10 22:33:35 -0500

    add @maxime.julian/ethereum-multicall package

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import AWSWebsocketProvider from '@aws/web3-ws-provider';
+import { Multicall, ContractCallResults, ContractCallContext } from '@maxime.julian/ethereum-multicall';
 import 'console.table';
 import { ethers } from 'ethers';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
@@ -57,6 +58,53 @@ const init = async () => {
         },
       })
     );
+
+    const multicall = new Multicall({ ethersProvider: provider, tryAggregate: true });
+
+    const contractCallContext: ContractCallContext<{ extraContext: string; foo4: boolean }>[] = [
+      {
+        reference: 'testContract',
+        contractAddress: '0x6795b15f3b16Cf8fB3E56499bbC07F6261e9b0C3',
+        abi: [
+          {
+            name: 'foo',
+            type: 'function',
+            inputs: [{ name: 'example', type: 'uint256' }],
+            outputs: [{ name: 'amounts', type: 'uint256' }],
+          },
+        ],
+        calls: [{ reference: 'fooCall', methodName: 'foo', methodParameters: [42] }],
+        context: {
+          extraContext: 'extraContext',
+          foo4: true,
+        },
+      },
+      {
+        reference: 'testContract2',
+        contractAddress: '0x66BF8e2E890eA0392e158e77C6381b34E0771318',
+        abi: [
+          {
+            name: 'fooTwo',
+            type: 'function',
+            inputs: [{ name: 'example', type: 'uint256' }],
+            outputs: [
+              { name: 'amounts', type: 'uint256' },
+              { name: 'path', type: 'address[]' },
+            ],
+          },
+        ],
+        calls: [{ reference: 'fooTwoCall', methodName: 'fooTwo', methodParameters: [42] }],
+      },
+    ];
+
+    const fn = async () => {
+      const results: ContractCallResults = await multicall.call(contractCallContext);
+      console.log(results);
+    };
+
+    fn();
+
+    return;
 
     // Instantiate exchange services
     const uniswapV2ExchangeService = new UniswapV2Exchange(provider);
