88f166c1d093e1e4c23632ccd6bbf1f1eb3655e6
Anthony Riera <anthony@slite.com>
2021-11-15 21:37:17 -0500

    updated server config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -87,32 +87,32 @@ module.exports = {
     dai: {
       ...baseOptions,
       'post-deploy': 'pm2 start ecosystem.config.js --env dai',
-      host: ['ec2-54-237-88-119.compute-1.amazonaws.com'],
+      host: ['ec2-3-80-38-132.compute-1.amazonaws.com'],
     },
     shib: {
       ...baseOptions,
       'post-deploy': 'pm2 start ecosystem.config.js --env shib',
-      host: ['ec2-18-207-139-178.compute-1.amazonaws.com'],
+      host: ['ec2-100-25-213-70.compute-1.amazonaws.com'],
     },
     link: {
       ...baseOptions,
       'post-deploy': 'pm2 start ecosystem.config.js --env link',
-      host: ['ec2-54-160-123-253.compute-1.amazonaws.com'],
+      host: ['ec2-54-234-74-21.compute-1.amazonaws.com'],
     },
     mana: {
       ...baseOptions,
       'post-deploy': 'pm2 start ecosystem.config.js --env mana',
-      host: ['ec2-52-201-212-7.compute-1.amazonaws.com'],
+      host: ['ec2-54-226-250-234.compute-1.amazonaws.com'],
     },
     aave: {
       ...baseOptions,
       'post-deploy': 'pm2 start ecosystem.config.js --env aave',
-      host: ['ec2-18-214-87-99.compute-1.amazonaws.com'],
+      host: ['ec2-34-227-193-72.compute-1.amazonaws.com'],
     },
     sand: {
       ...baseOptions,
       'post-deploy': 'pm2 start ecosystem.config.js --env sand',
-      host: ['ec2-54-165-201-252.compute-1.amazonaws.com'],
+      host: ['ec2-52-200-34-110.compute-1.amazonaws.com'],
     },
   },
 };
