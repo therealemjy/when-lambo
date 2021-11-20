4e1dae66b7ce6aad07d7879c2141ae7f8dec567e
therealemjy <maxime.julian.creations@gmail.com>
2021-11-20 11:43:20 -0500

    add basic contracts setup using Metacoin Truffle box

---
+++ contracts/ConvertLib.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.16 <0.9.0;
+
+library ConvertLib {
+  function convert(uint256 amount, uint256 conversionRate) public pure returns (uint256 convertedAmount) {
+    return amount * conversionRate;
+  }
+}
