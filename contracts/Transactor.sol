c4a342bd819f78ca01acb2f20b10cb83d8197793
therealemjy <maxime.julian.creations@gmail.com>
2021-11-21 22:05:03 -0500

    start implementing simple Uniswap trade on Transactor contract

--- contracts/Transactor.sol
+++ contracts/Transactor.sol
@@ -1,12 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './Owner.sol';
+import './interfaces/IUniswapV2Router.sol';
 
 contract Transactor is Owner {
-  address public uniswapV2RouterAddress;
+  IUniswapV2Router private uniswapV2Router;
 
   constructor(address _uniswapV2RouterAddress) {
-    uniswapV2RouterAddress = _uniswapV2RouterAddress;
+    // Initialize exchange contracts
+    uniswapV2Router = IUniswapV2Router(_uniswapV2RouterAddress);
+  }
+
+  function trade(
+    address _fromToken,
+    uint256 _fromTokenAmount,
+    address _toToken,
+    uint256 _minToTokenAmount,
+    uint256 _deadline
+  ) public owned {
+    IERC20(_fromToken).approve(address(uniswapV2Router), _fromTokenAmount);
+
+    // For now, we only handle simple paths from one token to another
+    address[] memory path = new address[](2);
+    path[0] = address(_fromToken);
+    path[1] = address(_toToken);
+
+    uniswapV2Router.swapExactTokensForTokens(_fromTokenAmount, _minToTokenAmount, path, msg.sender, _deadline);
   }
 }
