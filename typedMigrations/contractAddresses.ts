9bca0f13fba8e801f6c52261b5cf271784a11bf4
therealemjy <maxime.julian.creations@gmail.com>
2021-11-21 20:37:22 -0500

    add Ropsten Uniswap V2 router addres

--- typedMigrations/contractAddresses.ts
+++ typedMigrations/contractAddresses.ts
@@ -1,8 +1,9 @@
 export default {
   ropsten: {
-    uniswapV2Router: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+    uniswapV2Router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+    sushiswapRouter: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
   },
   mainnet: {
-    uniswapV2Router: '0x7A250D5630B4CF539739DF2C5DACB4C659F2488D',
+    uniswapV2Router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
   },
 };
