1405c3634e85cb15953f61b046bc74a9eea530f7
therealemjy <maxime.julian.creations@gmail.com>
2021-11-23 18:05:16 -0500

    put back logic to restart server every 30 seconds

--- index.ts
+++ index.ts
@@ -17,60 +17,69 @@ import { setupGlobalStateVariables } from './src/globalState';
 import logger from './src/logger';
 import handleError from './src/utils/handleError';
 
+const THIRTY_MINUTES_IN_MILLISECONDS = 1000 * 60 * 30;
+
+// Catch unhandled exceptions
+process.on('uncaughtException', (error) => {
+  handleError(error, true);
+  process.exit(1);
+});
+
 const init = async () => {
-  try {
-    const start = () => {
-      const provider = new ethers.providers.Web3Provider(
-        new AWSWebsocketProvider(config.aws.wsRpcUrl, {
-          clientConfig: {
-            maxReceivedFrameSize: 100000000, // bytes - default: 1MiB
-            maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
-            credentials: {
-              accessKeyId: config.aws.accessKeyId,
-              secretAccessKey: config.aws.secretAccessKey,
-            },
-            keepalive: true,
-            keepaliveInterval: 60000, // ms
-            // Enable auto reconnection
-            reconnect: {
-              auto: true,
-              delay: 5000, // ms
-              maxAttempts: 5,
-              onTimeout: false,
-            },
+  const start = () => {
+    const provider = new ethers.providers.Web3Provider(
+      new AWSWebsocketProvider(config.aws.wsRpcUrl, {
+        clientConfig: {
+          maxReceivedFrameSize: 100000000, // bytes - default: 1MiB
+          maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
+          credentials: {
+            accessKeyId: config.aws.accessKeyId,
+            secretAccessKey: config.aws.secretAccessKey,
           },
-        })
-      );
+          keepalive: true,
+          keepaliveInterval: 60000, // ms
+          // Enable auto reconnection
+          reconnect: {
+            auto: true,
+            delay: 5000, // ms
+            maxAttempts: 5,
+            onTimeout: false,
+          },
+        },
+      })
+    );
 
-      const multicall = new Multicall({ ethersProvider: provider, tryAggregate: true });
+    const multicall = new Multicall({ ethersProvider: provider, tryAggregate: true });
 
-      // Instantiate exchange services
-      const uniswapV2ExchangeService = new UniswapV2Exchange();
-      const sushiswapExchangeService = new SushiswapExchange();
-      const cryptoComExchangeService = new CryptoComExchange();
-      const kyberExchangeService = new KyberExchange();
+    // Instantiate exchange services
+    const uniswapV2ExchangeService = new UniswapV2Exchange();
+    const sushiswapExchangeService = new SushiswapExchange();
+    const cryptoComExchangeService = new CryptoComExchange();
+    const kyberExchangeService = new KyberExchange();
 
-      provider.addListener(
-        'block',
-        blockHandler({
-          multicall,
-          exchanges: [
-            uniswapV2ExchangeService,
-            sushiswapExchangeService,
-            kyberExchangeService,
-            cryptoComExchangeService,
-          ],
-        })
-      );
+    provider.addListener(
+      'block',
+      blockHandler({
+        multicall,
+        exchanges: [uniswapV2ExchangeService, sushiswapExchangeService, kyberExchangeService, cryptoComExchangeService],
+      })
+    );
 
-      logger.log('Price monitoring bot started.');
-    };
+    logger.log('Price monitoring bot started.');
 
-    // Start bot
-    start();
-  } catch (error: unknown) {
-    handleError(error);
-  }
+    // Regularly restart the bot so the websocket connection doesn't idle
+    setTimeout(() => {
+      logger.log('Restarting bot...');
+
+      // Shut down bot
+      provider.removeAllListeners();
+
+      start();
+    }, THIRTY_MINUTES_IN_MILLISECONDS);
+  };
+
+  // Start bot
+  start();
 };
 
 const server = http.createServer(function (req, res) {
@@ -113,10 +122,4 @@ server.listen(3000, async () => {
     eventEmitter.emit('error', err);
     process.exit(1);
   }
-});
-
-// Catch unhandled exceptions
-process.on('uncaughtException', (error) => {
-  handleError(error, true);
-  process.exit(1);
 });
