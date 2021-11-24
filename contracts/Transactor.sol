aae518cd229993fd3f94286fade3bfc2e1d95c9b
therealemjy <maxime.julian.creations@gmail.com>
2021-11-23 21:18:13 -0500

    WIP

--- contracts/Transactor.sol
+++ contracts/Transactor.sol
@@ -13,13 +13,24 @@ contract Transactor is Owner {
     uniswapV2Router = IUniswapV2Router(_uniswapV2RouterAddress);
   }
 
-  function getBalance() public view owned returns (uint256 balance) {
+  function getBalance(address _fromToken) public view owned returns (uint256 balance) {
+    return IERC20(_fromToken).balanceOf(address(this));
+  }
+
+  function getETHBalance() public view returns (uint256 balance) {
     return address(this).balance;
   }
 
   function withdraw(address _token, uint256 _tokenAmount) public owned {
     IERC20(_token).transfer(address(owner), _tokenAmount);
   }
+
+  // TODO: check if we need these functions to receive funds on the contract (if we receive ETH)
+  // Function to receive Ether. msg.data must be empty
+  receive() external payable {}
+
+  // Fallback function is called when msg.data is not empty
+  fallback() external payable {}
 
   function execute(
     address _fromToken,
@@ -28,6 +39,8 @@ contract Transactor is Owner {
     uint256 _minToTokenAmount,
     uint256 _deadline
   ) public owned {
+    require(IERC20(_fromToken).balanceOf(address(this)) >= _fromTokenAmount, 'Not enough fromToken on contract');
+
     IERC20(_fromToken).approve(address(uniswapV2Router), _fromTokenAmount);
 
     // For now, we only handle simple paths from one token to another
--- test/Transactor.test.ts
+++ test/Transactor.test.ts
@@ -12,23 +12,33 @@ contract('WhenLambo', (accounts) => {
   });
 
   it('should do a test transfer', async () => {
-    // Execute basic transaction
     const deployedTransactor = await Transactor.deployed();
 
     const accountBalance = await web3.eth.getBalance(accounts[0]);
+    const balance = await web3.eth.getBalance('0x573dB59bdc52A1Fdd45B11de9045904Ca6AFcBed');
     console.log('Account balance before test:', accountBalance);
+    console.log('Contract', balance.toString());
+
+    const contractBalance = await deployedTransactor.getETHBalance();
+
+    console.log('ETH on contract:', contractBalance.toString());
+
+    // Try withdrawing money
+    const res = await deployedTransactor.withdraw('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', web3.utils.toBN(3));
+
+    console.log('res', res);
 
     // Execute test transfer
-    await deployedTransactor.execute(
-      '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-      web3.utils.toBN(web3.utils.toWei('1', 'ether')),
-      '0x6b175474e89094c44da98b954eedeac495271d0f',
-      web3.utils.toBN(web3.utils.toWei('3000', 'ether')),
-      new Date(new Date().getTime() + 5 * 60000).getTime(),
-      { from: accounts[0] }
-    );
+    // await deployedTransactor.execute(
+    //   '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    //   web3.utils.toWei('1', 'ether'),
+    //   '0x6b175474e89094c44da98b954eedeac495271d0f',
+    //   web3.utils.toWei('3000', 'ether'), // Represents 3000 DAI decimals (sine DAI has 18 decimals like ETH)
+    //   new Date(new Date().getTime() + 5 * 60000).getTime(),
+    //   { from: accounts[0] }
+    // );
 
-    const updatedAccountBalance = await web3.eth.getBalance(accounts[0]);
-    console.log('Account balance after test:', updatedAccountBalance.toString());
+    // const updatedAccountBalance = await web3.eth.getBalance(accounts[0]);
+    // console.log('Account balance after test:', updatedAccountBalance.toString());
   });
 });
