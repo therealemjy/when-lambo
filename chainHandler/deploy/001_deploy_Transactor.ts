6cbfc60d915e1b567830336fde7e7da85ca77926
therealemjy <maxime.julian.creations@gmail.com>
2021-12-08 14:54:03 -0500

    test deployment to Rinkeby

--- chainHandler/deploy/001_deploy_Transactor.ts
+++ chainHandler/deploy/001_deploy_Transactor.ts
@@ -12,11 +12,14 @@ const deployFunc: DeployFunction = async function ({
   getNamedAccounts,
   ethers,
 }: HardhatRuntimeEnvironment) {
+  // Set these manually before doing any deployment
+  const maxPriorityFeePerGas = ethers.BigNumber.from('2000000000');
+  const maxFeePerGas = ethers.BigNumber.from('10000000000');
+
   const { ownerAddress } = await getNamedAccounts();
 
   await deploy('Transactor', {
     from: ownerAddress,
-    log: true,
     args: [
       WETH_MAINNET_ADDRESS,
       DYDX_SOLO_MAINNET_ADDRESS,
@@ -24,7 +27,10 @@ const deployFunc: DeployFunction = async function ({
       SUSHISWAP_ROUTER_MAINNET_ADDRESS,
       CRYPTO_COM_ROUTER_MAINNET_ADDRESS,
     ],
-    gasLimit: ethers.BigNumber.from('1780000'),
+    gasLimit: ethers.BigNumber.from('1780000'), // Roughly 30% more than the actual gas needed for the deployment (1362533)
+    maxPriorityFeePerGas,
+    maxFeePerGas,
+    log: true,
   });
 };
 
