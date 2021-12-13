b3e120877cabdfe14826790e596778d8fc15ac78
therealemjy <maxime.julian.creations@gmail.com>
2021-12-13 15:07:03 -0500

    clean up configs

--- logger/config.ts
+++ logger/config.ts
@@ -9,14 +9,12 @@ export interface EnvConfig {
   environment: Environment;
   isDev: boolean;
   isProd: boolean;
-  gasLimitMultiplicator: number;
 }
 
 const config: EnvConfig = {
   environment: (process.env.NODE_ENV as Environment) || 'development',
   isProd: (process.env.NODE_ENV as Environment) === 'production',
   isDev: (process.env.NODE_ENV as Environment) === 'development',
-  gasLimitMultiplicator: +env('GAS_LIMIT_MULTIPLICATOR'),
 };
 
 export default config;
