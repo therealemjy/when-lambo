c42eb3a23e2dcb12a8d64ebe026fcff44f5ed4f3
therealemjy <maxime.julian.creations@gmail.com>
2021-12-15 14:28:46 -0500

    house keeping (2)

--- utils/formatStrategies.ts
+++ utils/formatStrategies.ts
@@ -7,7 +7,7 @@ import multiplyAmounts from './multiplyAmounts';
 const formatStrategies = (parsedStrategies: ParsedStrategy[], borrowedAmountCount: number) =>
   parsedStrategies.map((parsedStrategy: ParsedStrategy) => ({
     borrowedWethAmounts: multiplyAmounts(
-      BigNumber.from(parsedStrategy.BORROWED_WETH_STARTING_MIDDLE_AMOUNT),
+      BigNumber.from(parsedStrategy.BORROWED_WETH_STARTING_BASE_AMOUNT),
       +parsedStrategy.BORROWED_WETH_AMOUNT_INCREMENT_PERCENT,
       borrowedAmountCount
     ),
